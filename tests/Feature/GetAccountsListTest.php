5774d084fa66032f9aed49b061f8a00f278b579b
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2023-12-24 15:44:15 +0500

    Refactor test

--- tests/Feature/GetAccountsListTest.php
+++ tests/Feature/GetAccountsListTest.php
@@ -15,37 +15,21 @@ class GetAccountsListTest extends TestCase
     {
         define('TRANSACTION_ID', '018eb91b-0ab2-72fc-bb09-ed9a58387bcd');
         define('ACCOUNT_ID1', '018eb91b-0ab0-7205-8c16-3ebae664c028');
+        define('ACCOUNT_ID2', '018eb91b-0ab2-72fc-bb09-ed9a5788e55c');
+        define('ACCOUNT_ID3', '018eb91b-0ab2-72fc-bb09-ed9a5805b0dc');
 
         $account_records = [
-            ['id' => ACCOUNT_ID1, 'name' => 'Счет Сбер', 'desc' => 'Зарплатный счет', 'type' => 'active'],
-            ['id' => '018eb91b-0ab2-72fc-bb09-ed9a5788e55c', 'name' => 'Зарплата', 'desc' => 'Оклад', 'type' => 'income'],
-            ['id' => '018eb91b-0ab2-72fc-bb09-ed9a5805b0dc', 'name' => 'Продукты', 'desc' => '', 'type' => 'expense'],
+            ['id' => ACCOUNT_ID1, 'name' => 'СчетСбер', 'type' => 'active', 'desc' => 'Зарплатный счет'],
+            ['id' => ACCOUNT_ID2, 'name' => 'Зарплата', 'type' => 'income', 'desc' => 'Оклад'],
+            ['id' => ACCOUNT_ID3, 'name' => 'Продукты', 'type' => 'expense', 'desc' => ''],
         ];
 
         \DB::table('accounts')->insert($account_records);
         \DB::table('transactions')->insert(['id' => TRANSACTION_ID]);
         \DB::table('entries')->insert([
-            [
-                'id' => Uuid::uuid7(),
-                'transaction_id' => TRANSACTION_ID,
-                'account_id' => ACCOUNT_ID1,
-                'type' => 'debit',
-                'amount' => 10000
-            ],
-            [
-                'id' => Uuid::uuid7(),
-                'transaction_id' => TRANSACTION_ID,
-                'account_id' => ACCOUNT_ID1,
-                'type' => 'debit',
-                'amount' => 5000
-            ],
-            [
-                'id' => Uuid::uuid7(),
-                'transaction_id' => TRANSACTION_ID,
-                'account_id' => ACCOUNT_ID1,
-                'type' => 'credit',
-                'amount' => 3000
-            ],
+            ['id' => Uuid::uuid7(), 'transaction_id' => TRANSACTION_ID, 'account_id' => ACCOUNT_ID1, 'type' => 'debit',  'amount' => 10000],
+            ['id' => Uuid::uuid7(), 'transaction_id' => TRANSACTION_ID, 'account_id' => ACCOUNT_ID1, 'type' => 'debit',  'amount' => 5000],
+            ['id' => Uuid::uuid7(), 'transaction_id' => TRANSACTION_ID, 'account_id' => ACCOUNT_ID1, 'type' => 'credit', 'amount' => 3000],
         ]);
 
         $response = $this->getJson('/accounts');
