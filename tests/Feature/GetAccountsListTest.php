ab32de0a8576c94a96e470ea3ad62a6004ef054e
Bulat R. Tukbaev <tukbaevbr@yandex.ru>
2023-12-24 15:35:52 +0500

    Refactor test

--- tests/Feature/GetAccountsListTest.php
+++ tests/Feature/GetAccountsListTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\AccountType;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
@@ -12,35 +11,38 @@ class GetAccountsListTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_get_accounts_list_with_balance()
+    public function get_accounts_list_with_balance()
     {
+        define('TRANSACTION_ID', '018eb91b-0ab2-72fc-bb09-ed9a58387bcd');
+        define('ACCOUNT_ID1', '018eb91b-0ab0-7205-8c16-3ebae664c028');
+
         $account_records = [
-            ['id' => '018eb91b-0ab0-7205-8c16-3ebae664c028', 'name' => 'Счет Сбер', 'desc' => 'Зарплатный счет', 'type' => 'active'],
+            ['id' => ACCOUNT_ID1, 'name' => 'Счет Сбер', 'desc' => 'Зарплатный счет', 'type' => 'active'],
             ['id' => '018eb91b-0ab2-72fc-bb09-ed9a5788e55c', 'name' => 'Зарплата', 'desc' => 'Оклад', 'type' => 'income'],
             ['id' => '018eb91b-0ab2-72fc-bb09-ed9a5805b0dc', 'name' => 'Продукты', 'desc' => '', 'type' => 'expense'],
         ];
 
         \DB::table('accounts')->insert($account_records);
-        \DB::table('transactions')->insert(['id' => '018eb91b-0ab2-72fc-bb09-ed9a58387bcd']);
+        \DB::table('transactions')->insert(['id' => TRANSACTION_ID]);
         \DB::table('entries')->insert([
             [
-                'id' => '018eb91b-0ab2-72fc-bb09-ed9a5909192c',
-                'transaction_id' => '018eb91b-0ab2-72fc-bb09-ed9a58387bcd',
-                'account_id' => '018eb91b-0ab0-7205-8c16-3ebae664c028',
+                'id' => Uuid::uuid7(),
+                'transaction_id' => TRANSACTION_ID,
+                'account_id' => ACCOUNT_ID1,
                 'type' => 'debit',
                 'amount' => 10000
             ],
             [
-                'id' => '018eb91b-0ab2-72fc-bb09-ed9a59aeea36',
-                'transaction_id' => '018eb91b-0ab2-72fc-bb09-ed9a58387bcd',
-                'account_id' => '018eb91b-0ab0-7205-8c16-3ebae664c028',
+                'id' => Uuid::uuid7(),
+                'transaction_id' => TRANSACTION_ID,
+                'account_id' => ACCOUNT_ID1,
                 'type' => 'debit',
                 'amount' => 5000
             ],
             [
-                'id' => '018eb91b-0ab2-72fc-bb09-ed9a5a65e8f2',
-                'transaction_id' => '018eb91b-0ab2-72fc-bb09-ed9a58387bcd',
-                'account_id' => '018eb91b-0ab0-7205-8c16-3ebae664c028',
+                'id' => Uuid::uuid7(),
+                'transaction_id' => TRANSACTION_ID,
+                'account_id' => ACCOUNT_ID1,
                 'type' => 'credit',
                 'amount' => 3000
             ],
